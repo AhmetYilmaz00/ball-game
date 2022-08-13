81040fbeb444244edd91cab5ed781950e946022f
Ahmet Yılmaz <ahmetyilmaz0964@gmail.com>
2022-08-13 17:39:37 +0300

    Add Hearth Icon

--- Assets/Game/Data-SO/GameSettings.cs
+++ Assets/Game/Data-SO/GameSettings.cs
@@ -5,5 +5,7 @@ namespace Game.Data_SO
     [CreateAssetMenu(fileName = "GameSettings", menuName = "GameSettings")]
     public class GameSettings : ScriptableObject
     {
+        [SerializeField] private float MaxHP;
+        [SerializeField] private float initialHP ;
     }
 }
